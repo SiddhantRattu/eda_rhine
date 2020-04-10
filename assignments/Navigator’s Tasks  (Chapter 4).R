66632789472be23c0f91785486d34326983bd9e4
Siddhant Rattu <sidrattu@gmail.com>
2020-04-10 14:31:29 +0530

    Update Navigator’s Tasks  (Chapter 4).R

--- assignments/Navigator’s Tasks  (Chapter 4).R
+++ assignments/Navigator’s Tasks  (Chapter 4).R
@@ -1,15 +1,19 @@
 # task 1
 library(data.table)
+list.file(data)
 library(ggplot2)
 library(moments)
 
-runoff_stats <- runoff_day[, .(mean_day = round(mean(value), 0),
+runoff_stats_tidy <- runoff_day[, .(mean_day = round(mean(value), 0),
                                sd_day = round(sd(value), 0),
                                min_day = round(min(value), 0),
                                max_day = round(max(value), 0)), by = sname]
-head(runoff_stats, 4)
 
-ggplot(runoff_stats, aes(x= sname, y=mean_day, col= sname ))+ geom_point()+
+runoff_stats_tidy <- melt(runoff_stats, id.vars = 'sname')
+
+head(runoff_stats_tidy, 4)
+
+ggplot(runoff_stats_tidy, aes(x= sname, y=mean_day, col= sname ))+ geom_point()+
        geom_point(size=2, shape=23)
          
 # task 2 
