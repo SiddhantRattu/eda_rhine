f8041bacedd062a70704887e5bdecb6f70f47dbe
Siddhant Rattu <sidrattu@gmail.com>
2020-06-14 20:30:59 +0530

    Create Stations.R

---
+++ Project/Stations.R
@@ -0,0 +1,35 @@
+
+library(data.table)
+
+runoff_eu_info <- readRDS("C:/Users/RIL/Documents/GitHub/eda_rhine/data/raw/runoff_eu_info.rds")
+
+runoff_eu_info
+
+# Ouestion 1
+
+#all stations are located in europe and russia 
+#France 
+#Czech Republic 
+#Lithuania
+#Russia
+#Ukraine
+
+# Question 2 
+# France - rivers - 1, stations- 2
+# Czech republic - rivers -3, stations-3
+# Lithuania - rivers- 1, stations- 1
+# Russia- rivers - 3, stations - 3
+# Ukraine - rivers -1, stations- 1
+
+# Question 3 
+#France two stations for loire river 
+#and other countries have 1 station per river 
+
+#Queston 4
+
+library(data.table)
+
+runoff_eu_info <- readRDS("C:/Users/RIL/Documents/GitHub/eda_rhine/data/raw/runoff_eu_info.rds")
+
+
+runoff_eu_info
